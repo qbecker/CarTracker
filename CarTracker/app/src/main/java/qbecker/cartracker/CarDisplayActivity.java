68bd79aaa5edb56f277e9d34a57f057a5511bda2
Quinten Becker <qbecker@Quintens-MacBook-Pro.local>
2017-05-30 17:13:11 -0700

    still working on adapter, saving place

--- CarTracker/app/src/main/java/qbecker/cartracker/CarDisplayActivity.java
+++ CarTracker/app/src/main/java/qbecker/cartracker/CarDisplayActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.ExpandableListView;
+
+import java.util.HashMap;
+import java.util.List;
 
 
 public class CarDisplayActivity extends AppCompatActivity {
@@ -12,14 +14,23 @@ public class CarDisplayActivity extends AppCompatActivity {
     private Button addRepairButton;
     private Button addTripButton;
     public ExpandableListView carListView;
+    public CarDisplayExpandableListAdapter carExpandListViewAdapter;
+
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_display);
+
+        carExpandListViewAdapter = new CarDisplayExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+
         addRepairButton = (Button) findViewById(R.id.addRepairButton);
         addTripButton = (Button) findViewById(R.id.addTripButton);
         carListView = (ExpandableListView) findViewById(R.id.individualCarListView);
+        carListView.setAdapter(carExpandListViewAdapter);
     }
 }
--- CarTracker/app/src/main/java/qbecker/cartracker/CarDisplayExpandableListAdapter.java
+++ CarTracker/app/src/main/java/qbecker/cartracker/CarDisplayExpandableListAdapter.java
@@ -1,49 +1,117 @@
 package qbecker.cartracker;
 
 import android.content.Context;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 
 /**
  * Created by qbecker on 5/30/17.
  */
 
-public class CarDisplayExpandableListAdapter extends BaseExpandableListAdapter {
-    Context parent;
+public class CarDisplayExpandableListAdapter extends BaseExpandableListAdapter{
 
-    public CarDisplayExpandableListAdapter(Context parent){
-        this.parent = parent;
+    public Context context;
+    public List<String> expandableListTitle;
+    public HashMap<String, List<String>> expandableListDetail;
+
+    public CarDisplayExpandableListAdapter(Context context, List<String> expandableListTitle,
+                                           HashMap<String, List<String>> expandableListDetail) {
+        this.context = context;
+
+        this.expandableListDetail = new HashMap<String, List<String>>();
+        this.expandableListTitle = expandableListTitle;
+        List<String> cricket = new ArrayList<String>();
+        cricket.add("India");
+        cricket.add("Pakistan");
+        cricket.add("Australia");
+        cricket.add("England");
+        cricket.add("South Africa");
+
+        List<String> football = new ArrayList<String>();
+        football.add("Brazil");
+        football.add("Spain");
+        football.add("Germany");
+        football.add("Netherlands");
+        football.add("Italy");
+
+        List<String> basketball = new ArrayList<String>();
+        basketball.add("United States");
+        basketball.add("Spain");
+        basketball.add("Argentina");
+        basketball.add("France");
+        basketball.add("Russia");
+
+        this.expandableListDetail.put("CRICKET TEAMS", cricket);
+        this.expandableListDetail.put("FOOTBALL TEAMS", football);
+        this.expandableListDetail.put("BASKETBALL TEAMS", basketball);
+        this.expandableListTitle = new ArrayList<String>(this.expandableListDetail.keySet());
 
     }
+
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .get(expandedListPosition);
+    }
+
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableListTitle.get(listPosition);
+    }
+
     @Override
     public int getGroupCount() {
-        return 0;
+        return this.expandableListTitle.size();
     }
 
     @Override
-    public int getChildrenCount(int groupPosition) {
-        return 0;
+    public long getGroupId(int listPosition) {
+        return listPosition;
     }
 
     @Override
-    public Object getGroup(int groupPosition) {
-        return null;
-    }
-
-    @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        return null;
-    }
-
-    @Override
-    public long getGroupId(int groupPosition) {
-        return 0;
-    }
-
-    @Override
-    public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+    public View getGroupView(int listPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+        return convertView;
     }
 
     @Override
@@ -52,17 +120,7 @@ public class CarDisplayExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
-    }
-
-    @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
     }
 }
